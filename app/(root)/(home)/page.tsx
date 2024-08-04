911d4be79e5db8ce331d656c274167c335b687bd
JINO-ROHIT <find.jinorohit@gmail.com>
2024-08-04 16:00:08 +0530

    navbar global search and navbar tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <h1> Suiii</h1>
-      <UserButton afterSignOutUrl="/"/>
+     
     </div>
-  );
+  )
 }
